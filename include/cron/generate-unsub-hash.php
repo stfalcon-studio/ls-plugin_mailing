1efd8b1c80099bc3bc5052283c06416957247473
vatseek <vatseek13@rambler.ru>
2012-11-15 10:43:09 +0200

    set  version indent

--- include/cron/generate-unsub-hash.php
+++ include/cron/generate-unsub-hash.php
@@ -7,7 +7,7 @@
  * @Description:
  * @Author: stfalcon-studio
  * @Author URI: http://stfalcon.com
- * @LiveStreet Version: 0.4.2
+ * @LiveStreet Version: 0.5.1
  * @License: GNU GPL v2, http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * ----------------------------------------------------------------------------
  */
