1d75cbdf6a1c7d387701d3ddbdb80627f62d1319
Igor Khokhlov <eld2303@gmail.com>
2013-01-18 12:25:20 +0200

    fix upd

--- update_from3.0.sql
+++ update_from3.0.sql
@@ -1,8 +1,6 @@
 ALTER TABLE `prefix_mailing` ADD COLUMN `mailing_talk` tinyint(1) unsigned NOT NULL DEFAULT '0';
-ALTER TABLE `prefix_mailing` ADD COLUMN `mailing_direct` tinyint(1) unsigned NOT NULL DEFAULT '0';
-
 ALTER TABLE `prefix_mailing_queue` ADD COLUMN `sended` tinyint(1) unsigned NOT NULL DEFAULT '0';
-
 UPDATE `prefix_mailing_queue` SET `sended` = '1' WHERE 1=1;
 
-ALTER TABLE `prefix_mailing` DROP `mailing_direct` ;
+ALTER TABLE `prefix_user` ADD `user_no_digest` TINYINT(1) NOT NULL DEFAULT '0';
+ALTER TABLE `prefix_user` ADD `user_no_digest_hash` VARCHAR(16) NOT NULL;
